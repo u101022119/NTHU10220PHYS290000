c37d33b5a1f645fae3dd16fc7213143890becad7
u101022171 <s101022171@m101.nthu.edu.tw>
2014-03-11 18:07:52 +0800

    HW1_EX2

---
+++ student/101022171/right_justify.py
@@ -0,0 +1,17 @@
+#EX2: Right justify
+#stu id: 101022171
+	
+from __future__ import print_function
+	
+def right_justify(s):
+    i = 0  
+    while i < (70-len(s)):
+        print (' ',end='')
+        i += 1
+    print (s)
+	
+while True:
+    x = raw_input("Input a string. (input 0 to end this program): ")
+    if x == '0':
+        break
+    right_justify(x)
