34d50c7a52412080dd60b8a66aae0e2761c67b78
9930125 <s9930125@m99.nthu.edu.tw>
2014-03-17 17:15:49 +0800

    Create drop_ball_height.py

---
+++ student/9930125/drop_ball_height.py
@@ -0,0 +1,7 @@
+h = float(raw_input('ENTER THE VALUE OF THE HEIGHT OF THE TOWER: '))
+t = float(raw_input('ENTER THE VALUE OF THE TIME INTERVAL: '))
+
+H = h - 0.5*9.8*t**2
+print "The height of the ball above the ground at time t after it is dropped is", H
+
+
