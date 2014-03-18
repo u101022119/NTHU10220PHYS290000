4515963b2d615cc9f46f998e441c6249f843a564
u101022116 <evanliu718@gmail.com>
2014-03-18 17:28:05 +0800

    Howework1 is done!!
    happy~~

---
+++ student/101022116/drop_ball_height.py
@@ -0,0 +1,6 @@
+g = 9.8
+h = input('Please enter h the heighy of the tower (meter):')
+t = input('Please enter t the time interval (second):')
+h_fin = h - 1.0/2.0 * g * t ** 2
+print 'the height of the ball above the groud at time t after it is drop :'
+print h_fin , 'meters'
