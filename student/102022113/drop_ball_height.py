4165f4a103d95000f6e1cd12798736a29a49ed05
u102022113 <amyjan0572@gmail.com>
2014-03-21 11:22:10 +0800

    Update drop_ball_height.py

--- student/102022113/drop_ball_height.py
+++ student/102022113/drop_ball_height.py
@@ -2,4 +2,10 @@ h=float(raw_input('the height of the tower (in meters) :'))
 t=float(raw_input('the time interval (in seconds) :'))
 s=h-(9.8*t**2)/2
 
-print 'The height of the ball above the ground is' , s , 'm'
+if h<=0 or t<=0:
+    print 'Both the height and the time have to be positive.'
+elif s<=0:
+    print 'The ball hits the ground.'
+else:
+    print 'The height of the ball above the ground is' , s , 'm'
+
