f7bc5b17683948c444ebb2ec6758687e80dad629
u102022113 <amyjan0572@gmail.com>
2014-03-21 11:39:38 +0800

    Update drop_ball_time.py

--- student/102022113/drop_ball_time.py
+++ student/102022113/drop_ball_time.py
@@ -1,6 +1,9 @@
 import math
 
 h=float(raw_input('the height of the tower (in meters) :'))
-t=math.sqrt(2*h/9.8)
 
-print 'It takes' , t , 'seconds for the ball to hit the ground.'
+if h<0:
+    print 'The hight cannot be negative.'
+else:
+    t=math.sqrt(2*h/9.8)
+    print 'It takes' , t , 'seconds for the ball to hit the ground.'
