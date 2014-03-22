4ee36b75507a2807fcba39abfae3455d0343f6f3
u101022110 <s101022110@m101.nthu.edu.tw>
2014-03-22 15:14:17 +0800

    factorial_rec

---
+++ student/101022110/factorial_rec.py
@@ -0,0 +1,15 @@
+def factorial_rec(n):
+    if n<0:
+        return 'factorial function is for positive numbers'
+    elif n-int(n)!=0:
+        return 'factorial function is for integers'
+    else:
+        if n==0:
+            return 1
+        else:
+            result=n*factorial_rec(n-1)
+            return result
+
+print "f(6)\n", factorial_rec(6)
+print "f(-2)\n", factorial_rec(-2)
+print "f(3.3)\n", factorial_rec(3.3)
