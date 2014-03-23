5bc802c2999a70f6f9fa028dd359066aea32cca7
u101022107 <cinnamoroll90806@yahoo.com.tw>
2014-03-24 01:09:09 +0800

    Hw1 7~10

---
+++ student/101022107/factorial_rec.py
@@ -0,0 +1,19 @@
+import math
+
+n=float(raw_input('n= '))
+
+def factorial_rec(n):
+    if int(math.fabs(n))!=n:
+        print 'n has to be a nonnegative integer'
+        return None
+    elif n==0:
+        return 1
+    else:
+        m=factorial_rec(n-1)
+        p=n*m
+        return p
+print factorial_rec(n)
+    
+
+    
+    
