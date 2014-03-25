4de31a7fd9b65a62b2bb701afe29e6ea0511da49
u101022127 <hooke1232006@yahoo.com.tw>
2014-03-25 23:56:36 +0800

    homework 1

--- student/100022127/drop_ball_height.py
+++
@@ -1,9 +0,0 @@
-h=int(raw_input('Enter the Height : '))
-t=int(raw_input('Enter the Time Interval : '))
-x=9.8/2*t**2
-
-
-if x>h:
-    print "The Height after",t," sec is : 0 ."
-else:
-    print "The Height after",t," sec is : ",h-x,"."
--- student/100022127/right_justify.py
+++
@@ -1,8 +0,0 @@
-def rightJustify(str):
-    for i in range(70-len(str)):
-        print '',
-    print str
-
-
-rightJustify("right_justify")
-rightJustify("0123456789112345678921234567893123456789412345678951234567896123456789")
