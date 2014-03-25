585620afa5df9e86fbd379ea96d53ce27c2fb799
u101022135 <qwerway123@hotmail.com>
2014-03-25 12:49:46 +0800

    HW1

---
+++ student/101022135/do_twice.py
@@ -0,0 +1,23 @@
+def do_twice(f):
+    f()
+    f()
+
+def print_spam():
+    print 'spam'
+
+def print_twice(s):
+    print s
+    print s
+
+def modified_print_twice( f , string ):
+    f(string)
+    f(string)
+
+def do_four( f , string ):
+    modified_print_twice( f , string )
+    modified_print_twice( f , string )
+
+do_twice(print_spam)
+print_twice('spam')
+modified_print_twice(print_twice , 'spam' )
+do_four(print_twice , 'spam' )
