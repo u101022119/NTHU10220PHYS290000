9b8314cf2680812393c024f5b3e37739f201d27c
u100022136 <lupk5951@gmail.com>
2014-04-15 18:06:32 +0800

    HW2 complete

--- student/100022136/madelung.py
+++ student/100022136/madelung.py
@@ -7,9 +7,9 @@ Created on Tue Apr 15 16:50:48 2014
 import math
 L=200
 M=0
-for i in range(-L,L):
-    for j in range(-L,L):
-        for k in range(-L,L):
+for i in range(-L,L+1):
+    for j in range(-L,L+1):
+        for k in range(-L,L+1):
             if(i==0 and j==0 and k==0):
                 M+=0
             elif((i+j+k)%2==0):
