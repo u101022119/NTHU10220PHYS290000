7c9d0c30f6911a65b13c6b9d833c691f2143dcff
u101022202 <single_83214@yahoo.com.tw>
2014-04-20 23:38:18 +0800

    HW2

--- student/101022202/HW2/mandelung.py
+++ student/101022202/HW2/mandelung.py
@@ -5,17 +5,18 @@ Spyder Editor
 This temporary script file is located here:
 C:\Users\Gary\.spyder2\.temp.py
 """
-import numpy as np
-import matplotlib.pyplot as plt
-def f1(t):
-    return t**2*np.exp(-t**2)
-def f2(t):
-    return t**2*f1(t)
-t = np.linspace(0,3,51)
-y1 = f1(t)
-y2 = f2(t)
-plt.plot(t,y1,'r-')
-plt.plot(t,y2,'bo')
-plt.xlabel('t')
-plt.ylabel('y')
-plt.show()
+import math
+L = 100
+M = 0
+for i in range(-L,L+1):
+    for j in range(-L,L+1):
+        for k in range(-L,L+1):
+            if(i == 0 and j == 0 and k == 0):
+                M = M+0
+            elif((i+j+k)%2 == 0):
+                M = M+1/math.sqrt(i**2+j**2+k**2)
+            else:
+                M = M-1/math.sqrt(i**2+j**2+k**2)
+
+print M
+                
