29b212564480a25382c815a88c66c5fbbc6f37cc
u101022202 <single_83214@yahoo.com.tw>
2014-04-27 17:26:49 +0800

    HW2

--- student/101022202/HW2/catalan.py
+++ student/101022202/HW2/catalan.py
@@ -6,17 +6,16 @@ This temporary script file is located here:
 C:\Users\user\.spyder2\.temp.py
 """
 
-def prime(n):
-    a = int(n**0.5)
-    b = range(a+1)
-    k = 0
-    for i in b[2:]:
-        if float(n/i) - int(n/i) == 0:
-            k = k+1
-    if k == 0:
-        print n
+def catalan(n):
+    if n == 0:
+        return 1
+    else:
+        return int((4*n-2.0)/(n+1.0)*catalan(n-1))
                 
-prime(23)
+a = int(raw_input('Put an integer:'))
+for i in range(a+1):
+    print catalan(i),
+
 
         
         
