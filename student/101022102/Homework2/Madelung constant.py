165b8825f3eb5620f13d38328aca809d1622075d
u101022102 <ejp168@gmail.com>
2014-04-28 23:28:16 +0800

    Homework 2

---
+++ student/101022102/Homework2/Madelung constant.py
@@ -0,0 +1,20 @@
+import math
+def madelung(l):
+    a=0
+    for i in range(1,l+1):
+        for j in range (1,l+1):
+            for k in range(1,l+1):
+                a = (-1)**(i+j+k+1)/math.sqrt(i**2+j**2+k**2)
+    b=0
+    i=0
+    for j in range (1,l+1):
+            for k in range(1,l+1):
+                b = (-1)**(j+k+1)/math.sqrt(j**2+k**2)
+    c=0
+    i=0
+    j=0
+    for k in range(1,l+1):
+                a = (-1)**(k+1)/math.sqrt(k**2)
+    m = a*8+b*12+c*6
+    print m
+    
