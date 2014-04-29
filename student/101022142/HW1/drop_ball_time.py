48d954b198609e1d6c21e483a1c08ce0aa23ae39
u101022142 <s101022142@m101.nthu.edu.tw>
2014-04-29 17:02:02 +0800

    new

---
+++ student/101022142/HW1/drop_ball_time.py
@@ -0,0 +1,7 @@
+#1-4-2.drop_ball_time.py
+h=float(raw_input("please tell the height of the ball in meter"))
+print "the time the ball drop from the height is"
+t=(2*h/9.81)**0.5
+print t,'second'
+
+#or import "math",use math.sqrt()
