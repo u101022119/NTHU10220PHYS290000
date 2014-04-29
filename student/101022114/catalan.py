44281779fe8640dcc309683b1b2c5df5537d5e6c
u101022114 <h0001232000@yahoo.com.tw>
2014-04-29 15:34:57 +0800

    hw2

---
+++ student/101022114/catalan.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This temporary script file is located here:
+C:\Users\Helen\.spyder2\.temp.py
+"""
+
+def m(i,j,k):
+    m=(-1.0)**(i+j+k)/((i**0.5+j**0.5+k**0.5)**0.5)
+    
+        
