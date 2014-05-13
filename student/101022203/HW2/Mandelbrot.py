231f1d958b532c6ef67afa425b9b7d889696acda
u101022203 <roy29383@yahoo.com.tw>
2014-05-13 08:12:06 +0800

    HW2

---
+++ student/101022203/HW2/Mandelbrot.py
@@ -0,0 +1,30 @@
+from math import *
+from pylab import *
+	
+	
+x=arange(-2,2,0.02)
+y=arange(-2,2,0.02)
+	
+real=[]
+im=[]
+x1=empty([len(x),len(y)],float)
+for i in range(len(x)):
+    for j in range(len(y)):
+	c=complex(x[i],y[i])
+	z=0+0j
+	for k in range(100):
+	    z=z**2+c
+	        if abs(z)>2:
+	        x1[j,i]=k
+	        break
+	
+	    if abs(z)<=2:
+	        x1[j,i]=abs(z)*100
+	        real.append(x[i])
+	        im.append(y[j])
+plot(real,im,"k")
+imshow(x1,origin="O",extent=[-2,2,-2,2])
+hot()
+show()
+
+
