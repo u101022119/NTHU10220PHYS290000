c08ed0793d7ca0746a134da269149cf5c944b5f4
u101022220 <pupai22@yahoo.com.tw>
2014-05-27 16:49:06 +0800

    HW3

---
+++ student/101022220/HW3/sorting.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 27 16:48:09 2014
+
+@author: user
+"""
+
+import random
+def random_list(N,a,b):
+    c = []
+    for i in range(N):
+        c.append(random.randint(a,b))
+    return c
+
+def selection_sort(c,N):
+    print random.sample(c,N)
+
+def insertion_sort(c,N):
+    for i in range(N):
+        c.append(random.randint(a,b))
+    print c
+def bubble_sort(c):
+    for i in range(len(c)):
+        for j in range(len(c)-1):
+            if c[j] < c[j+1]:
+                temp = c[j]
+                c[j] = c[j+1]
+                c[j+1] = temp
+    print c
+
+a = 0
+b = 100
+insertion_sort(random_list(6,0,100),10)
+bubble_sort(random_list(6,0,100))
+
