acd2cf31ddf2df3ba555ce8dcb14ca4e4fdd4ca3
u101020021 <mas581301@gmail.com>
2014-05-28 19:47:13 +0800

    HW3_sort

--- student/101020021/hw3/sorting.py
+++ student/101020021/hw3/sorting.py
@@ -5,7 +5,12 @@ Created on Tue May 20 16:38:35 2014
 @author: user
 """
 
-import random
+#import random
+from random import randint
+import time
+import copy
+import numpy as np
+from pylab import *
 
 def random_list(N,a,b):
     lst=[]
@@ -15,6 +20,7 @@ def random_list(N,a,b):
 
 
 def selection_sort(lst):
+    begin=time.time()
     for i in range(len(lst)):
         min_o=i
         for j in range(i,len(lst)):
@@ -24,9 +30,12 @@ def selection_sort(lst):
         a=lst[i]
         lst[i]=lst[min_o]
         lst[min_o]=a
+    end=time.time()
+    return lst,(end-begin)
     
     
 def insertion_sort(lst):
+    begin=time.time()
     for i in range(len(lst)):
         for j in range(i):
             if lst[0]>lst[i]:
@@ -41,36 +50,98 @@ def insertion_sort(lst):
                     lst[i-k]=lst[i-k-1]
                 lst[j+1]=a
                 break
+    end=time.time()
+    return lst,(end-begin)
             
 def bubble_sort(lst):
+    begin=time.time()
     for i in range(len(lst)):
         for j in range(len(lst)-i-1):
             if lst[j]>lst[j+1]:
                 a=lst[j+1]
                 lst[j+1]=lst[j]
                 lst[j]=a
+    end=time.time()
+    return lst,(end-begin)
     
     
 def quick_sort(lst):
+    begin=time.time()
     c=0
     for i in range(len(lst)):
         b=0
         for j in range(len(lst)-i-1):
+            if j==len(lst)-i-2:
+                b=1
+                c=j
             if lst[j]>lst[-i-1]:
                 a=lst[j]
                 lst[j]=lst[-i-2]
                 lst[-i-2]=lst[-i-1]
                 lst[-i-1]=a
+                c=j-1
                 break
-            elif j==len(lst)-i-2:
-                b=1
-                c=j+1                
+        if b and ( 0<c ):
+            lst[:c+1]=quick_sort(lst[:c+1])
+        if b and ( c<(len(lst)-3) ):
+            lst[2+c:]=quick_sort(lst[2+c:])
         if b:
             break
-    quick_sort(lst[:c+1])
-    quick_sort(lst[3+c:])
-    
-lst=random_list(10,0,10)
-print lst
-quick_sort(lst)
-print lst
+    end=time.time()
+    return lst,(end-begin)
+
+
+
+def build_in_sorted(lst):
+    begin=time.time()
+    sorted(lst)
+    end=time.time()
+    return lst,(end-begin)
+
+
+N=200
+
+x = arange(0,N,1)
+y_1 = np.zeros(N)
+y_2 = np.zeros(N)
+y_3 = np.zeros(N)
+y_4 = np.zeros(N)
+y_5 = np.zeros(N)
+a=[]
+b=[]
+c=[]
+d=[]
+e=[]
+lst=[]
+
+for i in arange(1,N):
+    lst=random_list(i,0,100)
+    a=lst[:]#=copy.copy(lst)
+    b=lst[:]#=copy.copy(lst)
+    c=lst[:]#=copy.copy(lst)
+    d=lst[:]#=copy.copy(lst)
+    e=lst[:]#=copy.copy(lst)
+    a,y_1[i]=selection_sort(a)
+    b,y_2[i]=insertion_sort(b)
+    c,y_3[i]=bubble_sort(c)
+    d,y_4[i]=quick_sort(d)
+    e,y_5[i]=build_in_sorted(e)
+
+xlabel ('size')
+ylabel ('time')
+plot(x,y_1)
+plot(x,y_2)
+plot(x,y_3)
+plot(x,y_4)
+plot(x,y_5)
+
+#loglog(x,y_1)
+#loglog(x,y_2)
+#loglog(x,y_3)
+#loglog(x,y_4)
+#loglog(x,y_5)
+
+legend(['selecion', 'insertion', 'bubble','quick', 'build_in'])
+show()
+
+
