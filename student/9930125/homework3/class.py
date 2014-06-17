6c76db87d0abb784a47dd1b8d65db8bfc9ee439e
9930125 <s9930125@m99.nthu.edu.tw>
2014-06-17 21:25:33 +0800

    commit

--- student/9930125/homework3/class.py
+++ student/9930125/homework3/class.py
@@ -16,9 +16,9 @@ def move_rectangle(a,dx,dy):
 p = Point()
 p.x1 = 0
 p.y1 = 0
-p.x2 = 3
-p.y2 = 4
-print distance_between_point(p.x1,p.y1,p.x2,p.y2)
+p.x2 = 7
+p.y2 = 24
+print 'd =', distance_between_point(p.x1,p.y1,p.x2,p.y2)
 
 box = Rectangle()
 box.corner = Point()
@@ -30,7 +30,8 @@ box.corner.y = 200
 box2.corner.x = 100
 box2.corner.y = 200
 
-move_rectangle(box.corner,20,30)
-print box.corner.x,box.corner.y
+move_rectangle(box.corner,40,50)
+print 'x1, y1 become', box.corner.x,box.corner.y
 
-move_rectangle(box2.corner,40,60)
+move_rectangle(box2.corner,80,100)
+print 'x2, y2 become', box2.corner.x,box2.corner.y
