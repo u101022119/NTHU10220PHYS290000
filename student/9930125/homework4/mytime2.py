2b3b95fed61ef447dcf6f10bd78a6d38e03f80f7
9930125 <s9930125@m99.nthu.edu.tw>
2014-06-17 23:56:36 +0800

    finally!

--- student/9930125/homework4/mytime2.py
+++ student/9930125/homework4/mytime2.py
@@ -1,63 +1,37 @@
-class Time():
-    def __init__ (self,h,m,s):
-        self.hour = h
-        self.minute = m
-        self.second = s
-    def to_second(self):
-        s = self.hour * 3600
-        s += self.minute * 60
-        s += self.second * 1
-        return s
-    def print_time(self):
-        a,b,c = self.hour,self.minute,self.second
-        print '{0:02d} : {1:02d} : {2:02d}'.format(a,b,c)
-    def is_after(t1,t2):
-        th = t1.hour > t2.hour
-        tm = t1.hour == t2.hour and t1.minute > t2.minute
-        ts = t1.hour == t2.hour and t1.minute == t2.minute and t1.second > t2.second
-        ans = th or tm or ts
-        print ans
-        return ans
-    def increment_modifier(self,s):
-        s = int(s) + self.to_second()
-        h = s/3600 
-        s = s%3600
-        m = s/60 
-        s = s%60
-        self.hour , self.minute , self.second = h,m,s
-        self.print_time()
-        return self
-    def increment_pure(self,s):
-        s = int(s) + self.to_second()
-        h = s/3600 
-        s = s%3600
-        m = s/60 
-        s = s%60
-        ans = Time(0,0,0)
-        ans.hour , ans.minute , ans.second = h,m,s
-        self.print_time()
-        ans.print_time()
-        return ans
-    def mul_time(self,a):
-        s = self.to_second()
-        s *= a
-        ans = Time(0,0,0)
-        ans.increment_modifier(s)
-        return ans
-
-    def __cmp__(self,T2):
-        t1, t2 = self.to_second(), T2.to_second()
-        if t1 > t2:
-            return 1
-        elif t1 < t2:
+class Time(object):
+    def __cmp__(self,other):
+        if self.hour > other.hour:
+            print 'Time1 is ahead Time2'
+            return 1 
+        elif self.hour < other.hour:
+            print 'Time2 is ahead Time1'
             return -1
         else:
-            return 0
+            if self.minute > other.minute:
+                print 'Time1 is ahead Time2'
+                return 1
+            elif self.minute < other.minute:
+                print 'Time2 is ahead Time1'
+                return -1
+            else:
+                if self.second > other.second:
+                    print 'Time1 is ahead Time2'
+                    return 1
+                elif self.second < other.second:
+                    print 'Time2 is ahead Time1'
+                    return -1
+                else:
+                    print 'Time1 = Time2'
+                    return 0
 
-T = Time(8,0,0)
-T2 = Time(8,1,0)
-T3 = Time(8,1,2)
-T.print_time()
-T2.print_time()
-T3.print_time()
-print T > T2 , T == T2 , T < T2
+Time1 = Time()
+Time2 = Time()
+
+Time1.hour = float(raw_input('Time1.hour: '))
+Time1.minute = float(raw_input('Time1.minute: '))
+Time1.second = float(raw_input('Time1.second: '))
+Time2.hour = float(raw_input('Time2.hour: '))
+Time2.minute = float(raw_input('Time2.minute: '))
+Time2.second = float(raw_input('Time2.second: '))
+
+cmp(Time1,Time2)
