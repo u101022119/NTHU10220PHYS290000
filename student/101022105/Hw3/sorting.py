9a4d7a6f176c79e67cff11dfd90577621a030c66
u101022105 <harry456sonic@gmail.com>
2014-06-17 13:30:57 +0800

    Hw3 and Hw4

---
+++ student/101022105/Hw3/sorting.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 17 13:21:47 2014
+
+@author: Sonic
+"""
+
+from random import *
+from time import time
+from pylab import *
+
+def random_list(N):
+    array=[]
+    for i in range (0,N):
+        array.append(randint(0,N-1))
+    return array
+    
+def selection_sort(array):
+    for i in range (0,len(array)):
+        min = i
+        for j in range(i+1,len(array)):
+            if array[j] < array[min]:
+                min = j
+        array[i], array[min] = array[min], array[i]
+        
+def insertion_sort(array):
+    for i in range(1,len(array)):
+        save = array[i]
+        j = i
+        while j > 0 and array[j-1] > save:
+            array[j] = array[j-1]
+            j -= 1
+        array[j] = save
+        
+def bubble_sort(array):
+    done = False
+    while not done:
+        done = True
+        for i in range(len(array)-1,0,-1):
+            if array[i] < array[i-1]:
+                array[i], array[i-1] = array[i-1], array[i]
+                done = False
+
+def quick_sort_list(array):
+    if array == []:
+        return []
+    else:
+        pivot = array[0]
+        lesser = quick_sort_list([x for x in array[1:] if x < pivot])
+        greater = quick_sort_list([x for x in array[1:] if x >= pivot])
+        return lesser+[pivot]+greater
